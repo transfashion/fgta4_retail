fc2181468d324354fc3f57f7c04d21cf39539044
agungdhewe <agung_dhewe@yahoo.com>
2024-09-04 09:23:02 +0700

    add sync data pool

---
+++ fashion/merchsync/_genconfig.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const dbtype = global.dbtype;
+const comp = global.comp;
+
+module.exports = {
+	title: "Syncronize Document",
+	autoid: false,
+	
+	persistent: {
+		'fsn_merchsync' : {
+			primarykeys: ['merchsync_id'],
+			comment: 'Daftar Season',
+			data: {
+				merchsync_id: {text:'ID', type: dbtype.varchar(30), null:false, uppercase: true, options:{required:true,invalidMessage:'ID harus diisi'}},
+				merchsync_doc: {text:'Document', type: dbtype.varchar(90), null:false, unset:true},
+				merchsync_type: {text:'Type', type: dbtype.varchar(30), null:false, unset:true},
+				merchsync_isprocessing: {text:'Processing', type: dbtype.boolean, unset:true, null:false, default:0},
+				merchsync_batch: {text:'Batch', type: dbtype.varchar(30), null:true, unset:true},
+				merchsync_isfail: {text:'Processing', type: dbtype.boolean, unset:true, null:false, default:0},
+				merchsync_result: {text:'Result', type: dbtype.varchar(30), null:false, unset:true}
+			},
+			uniques : {},
+		},
+	},
+
+	schema: {
+		title: 'Syncronize Document',
+		header: 'fsn_merchsync',
+		detils: {
+		}
+	}
+
+}
+
+
+
---
+++ fashion/merchsync/merchsync.sql
@@ -0,0 +1,46 @@
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- drop table if exists `fsn_merchsync`;
+
+
+CREATE TABLE IF NOT EXISTS `fsn_merchsync` (
+	`merchsync_id` varchar(30) NOT NULL , 
+	`merchsync_doc` varchar(90) NOT NULL , 
+	`merchsync_type` varchar(30) NOT NULL , 
+	`merchsync_isprocessing` tinyint(1) NOT NULL DEFAULT 0, 
+	`merchsync_batch` varchar(30)  , 
+	`merchsync_isfail` tinyint(1) NOT NULL DEFAULT 0, 
+	`merchsync_result` varchar(30) NOT NULL , 
+	`_createby` varchar(14) NOT NULL , 
+	`_createdate` datetime NOT NULL DEFAULT current_timestamp(), 
+	`_modifyby` varchar(14)  , 
+	`_modifydate` datetime  , 
+	PRIMARY KEY (`merchsync_id`)
+) 
+ENGINE=MyISAM
+COMMENT='Daftar Que Dokumen yang akan diSync';
+
+
+ALTER TABLE `fsn_merchsync` ADD COLUMN IF NOT EXISTS  `merchsync_doc` varchar(90) NOT NULL  AFTER `merchsync_id`;
+ALTER TABLE `fsn_merchsync` ADD COLUMN IF NOT EXISTS  `merchsync_type` varchar(30) NOT NULL  AFTER `merchsync_doc`;
+ALTER TABLE `fsn_merchsync` ADD COLUMN IF NOT EXISTS  `merchsync_isprocessing` tinyint(1) NOT NULL DEFAULT 0 AFTER `merchsync_type`;
+ALTER TABLE `fsn_merchsync` ADD COLUMN IF NOT EXISTS  `merchsync_batch` varchar(30)   AFTER `merchsync_isprocessing`;
+ALTER TABLE `fsn_merchsync` ADD COLUMN IF NOT EXISTS  `merchsync_isfail` tinyint(1) NOT NULL DEFAULT 0 AFTER `merchsync_batch`;
+ALTER TABLE `fsn_merchsync` ADD COLUMN IF NOT EXISTS  `merchsync_result` varchar(30) NOT NULL  AFTER `merchsync_isfail`;
+
+
+ALTER TABLE `fsn_merchsync` MODIFY COLUMN IF EXISTS  `merchsync_doc` varchar(90) NOT NULL   AFTER `merchsync_id`;
+ALTER TABLE `fsn_merchsync` MODIFY COLUMN IF EXISTS  `merchsync_type` varchar(30) NOT NULL   AFTER `merchsync_doc`;
+ALTER TABLE `fsn_merchsync` MODIFY COLUMN IF EXISTS  `merchsync_isprocessing` tinyint(1) NOT NULL DEFAULT 0  AFTER `merchsync_type`;
+ALTER TABLE `fsn_merchsync` MODIFY COLUMN IF EXISTS  `merchsync_batch` varchar(30)    AFTER `merchsync_isprocessing`;
+ALTER TABLE `fsn_merchsync` MODIFY COLUMN IF EXISTS  `merchsync_isfail` tinyint(1) NOT NULL DEFAULT 0  AFTER `merchsync_batch`;
+ALTER TABLE `fsn_merchsync` MODIFY COLUMN IF EXISTS  `merchsync_result` varchar(30) NOT NULL   AFTER `merchsync_isfail`;
+
+
+
+
+
+
+
+
+
