c462ce193988b9dfd7924be9c086d77ab714e717
agung-transfashion <>
2025-07-03 16:44:14 +0700

    update cmdscript

--- merch/merchorderout/apis/xtion-importregtopo.php
+++ merch/merchorderout/apis/xtion-importregtopo.php
@@ -33,7 +33,8 @@ $API = new class extends merchorderoutBase {
 		$logfile = "/mnt/ramdisk/log-$dt-$pid.txt";
 		$clipath = __LOCALCLIENT_DIR . '/cli';
 
-		$cmdscript = "/var/www/fgtacloud4u/server_apps/retail/merch/merchorderout/cli/importregtopo.sh";
+		// $cmdscript = "/home/ubuntu/fgtamodules/fgta4_retail/merch/merchorderout/cli/importregtopo.sh";
+		$cmdscript = "$clipath/importregtopo.sh";
 		$command = "$cmdscript -c \"$clipath\" -n $name -p $pid -u $username -d \"$data\" -l \"$logfile\" 2>&1 | tee -a $logfile 2>/dev/null >/dev/null &";
 		shell_exec($command);
 
